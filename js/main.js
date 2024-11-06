2bef85007f7b412d3c7bace036aae655f175cd45
NestorAdrian18 <nestorbns18@gmail.com>
2024-11-06 21:04:09 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menupindot = document.querySelector(".burger");
+const menupopup = document.querySelector(".menu");
+
+menupindot.addEventListener("click", ()=> {
+    menupopup.classList.toggle("active");
+    menupindot.classList.toggle("active");
+});
